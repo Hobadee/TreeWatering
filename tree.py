9d4c7df621409b8976c25cd4598f3717e79788d9
Eric Kincl <eric@kincl.net>
2018-11-25 02:42:52 -0800

    Lots of changes to tree.py
    Now most basic functions largely work.  (From a software perspective... having issues with tree water monitoring hardware implementation.)
    This manual adding of water via the button works.  It will also check for low-water in the pump and stop if the pump runs out.  There are some issues with this, although I suspect it's a hardware design issue as well.  (Possibly PWM screwing things up.)

--- tree.py
+++ tree.py
@@ -1,74 +1,222 @@
 #!/usr/bin/python3
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
-from gpiozero import LEDBarGraph, LED, Button
-from signal import pause
+from gpiozero import LEDBarGraph, LED, Button, DigitalInputDevice, SmoothedInputDevice, DigitalOutputDevice
+#from signal import pause
 from time import sleep
 
+import signal
 import time
 import atexit
+import os
 
-GPIO_led_R1 = 14
-GPIO_led_Y1 = 15
-GPIO_led_G1 = 4
-GPIO_led_B1 = 17
-GPIO_btn_B1 = 18
-MTR_pump1 = 1
+class treeWater:
+        def __init__(self):
+                self.GPIO_led_R1 = 14
+                self.GPIO_led_Y1 = 15
+                self.GPIO_led_G1 = 4
+                self.GPIO_led_B1 = 17
+                self.GPIO_btn_B1 = 18
+                self.MTR_pump1 = 1
+                
+                self.GPIO_wtr_tgd = 21
+                self.GPIO_wtr_t01 = 20
+                self.GPIO_wtr_t02 = 19
+                self.GPIO_wtr_t03 = 16
+                self.GPIO_wtr_t04 = 13
+                self.GPIO_wtr_t05 = 12
+                self.GPIO_wtr_t06 = 6
+                self.GPIO_wtr_t07 = 5
+                self.GPIO_wtr_t08 = 7
+                self.GPIO_wtr_t09 = 8
+                self.GPIO_wtr_t10 = 11
+                self.GPIO_wtr_t11 = 9
+                self.GPIO_wtr_t12 = 10
+                self.GPIO_wtr_t13 = 25
+                
+                self.GPIO_wtr_pgd = 22
+                self.GPIO_wtr_p01 = 23
+                
+                # Setup the water level indicator
+                self.wtrLevel = LEDBarGraph(self.GPIO_led_R1, self.GPIO_led_Y1, self.GPIO_led_G1, pwm=True)
+                
+                # Setup the Pump Water Level Sensor
+                # _pumpWtrLevelGnd should remain ON the entire time
+                self._pumpWtrLevelGnd = DigitalOutputDevice(self.GPIO_wtr_pgd, True, True)
+                self.pumpWtrLevel = DigitalInputDevice(self.GPIO_wtr_p01)
+                
+                # Setup the tree water level indicator
+                # _treeWtrLevelGnd should remain ON the entire time
+                self._treeWtrLevelGnd = DigitalOutputDevice(self.GPIO_wtr_tgd, True, True)
 
-wtrLevel = LEDBarGraph(GPIO_led_R1, GPIO_led_Y1, GPIO_led_G1, pwm=True)
-pumpSts = LED(GPIO_led_B1)
-pumpBtn = Button(GPIO_btn_B1, True)
+                self.treeWtrLevel01 = DigitalInputDevice(self.GPIO_wtr_t01)
+                self.treeWtrLevel02 = DigitalInputDevice(self.GPIO_wtr_t02)
+                self.treeWtrLevel03 = DigitalInputDevice(self.GPIO_wtr_t03)
+                self.treeWtrLevel04 = DigitalInputDevice(self.GPIO_wtr_t04)
+                self.treeWtrLevel05 = DigitalInputDevice(self.GPIO_wtr_t05)
+                self.treeWtrLevel06 = DigitalInputDevice(self.GPIO_wtr_t06)
+                self.treeWtrLevel07 = DigitalInputDevice(self.GPIO_wtr_t07)
+                self.treeWtrLevel08 = DigitalInputDevice(self.GPIO_wtr_t08)
+                self.treeWtrLevel09 = DigitalInputDevice(self.GPIO_wtr_t09)
+                self.treeWtrLevel10 = DigitalInputDevice(self.GPIO_wtr_t10)
+                self.treeWtrLevel11 = DigitalInputDevice(self.GPIO_wtr_t11)
+                self.treeWtrLevel12 = DigitalInputDevice(self.GPIO_wtr_t12)
+                self.treeWtrLevel13 = DigitalInputDevice(self.GPIO_wtr_t13)
+                
+                # Setup the pump status indicator and manual override button
+                self.pumpStsLED = LED(self.GPIO_led_B1)
+                self.pumpBtn = Button(self.GPIO_btn_B1, True)
+                
+                # create a default object, no changes to I2C address or frequency
+                self.mtrHat = Adafruit_MotorHAT(addr=0x60)
+                
+                self.pump = self.mtrHat.getMotor(self.MTR_pump1)
+                
+                # We should always run at max speed
+                self.pump.setSpeed(255)
+                
+                self.pumpSts = False
 
-# create a default object, no changes to I2C address or frequency
-mtrHat = Adafruit_MotorHAT(addr=0x60)
+        # recommended for auto-disabling motors on shutdown!
+        def Shutdown(self):
+	        self.mtrHat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	        self.mtrHat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	        self.mtrHat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	        self.mtrHat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
-pump = mtrHat.getMotor(MTR_pump1)
+        def pumpRun(self):
+                if self.pumpSts == False:
+                        # Pump is off; Turn it on
+                        self.log("Turning Pump On","INFO")
+                        self.pump.run(Adafruit_MotorHAT.FORWARD)
+                        self.pumpSts = True
+                        self.pumpStsLED.on()
+                #else:
+                        # Pump is already running; Do nothing
 
-# set the speed to start, from 0 (off) to 255 (max speed)
-pump.setSpeed(255)
+        def pumpStop(self):
+                if self.pumpSts == True:
+                        # Pump is on; Turn it off
+                        self.log("Turning Pump Off","INFO")
+                        self.pump.run(Adafruit_MotorHAT.RELEASE)
+                        self.pumpSts = False
+                        self.pumpStsLED.off()
+                #else:
+                        # Pump is already off; Do nothing
 
-# recommended for auto-disabling motors on shutdown!
-def Shutdown():
-	mtrHat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-	mtrHat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-	mtrHat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-	mtrHat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+        def log(self, msg, level = "INFO"):
+                # Probably prefix MSG with timestamp?
+                # Add logging facilities later
+                ts = time.gmtime()
+                ts = time.strftime("%Y-%m-%d %H:%M:%S", ts)
+                print(ts," - ",level,": ",msg)
 
-def pumpRun():
-        log("Turning Pump On")
-        pump.run(Adafruit_MotorHAT.FORWARD)
-        pumpSts.on()
+        def manualRun(self):
+                self.log("Manual override process running...")
+                while True:
+                        if self.pumpBtn.value and self.pumpWtrLevel.value:
+                                self.pumpRun()
+                        else:
+                                self.pumpStop()
+                        # Cool this down a little
+                        sleep(0.5)
 
-def pumpStop():
-        log("Turning Pump Off")
-        pump.run(Adafruit_MotorHAT.RELEASE)
-        pumpSts.off()
+        def monitorWtrPump(self):
+                self.log("Pump water level checker running...")
+                lastState = not self.pumpWtrLevel.value
+                stateCheck = True
+                while True:
+                        state = self.pumpWtrLevel.value
+                        if lastState != state:
+                                lastState = state
+                                if state:
+                                        self.log("Pump water present","INFO")
+                                        if self.pumpSts:
+                                                self.pumpStsLED.on()
+                                        else:
+                                                self.pumpStsLED.off()
+                                else:
+                                        self.log("Low pump water level","WARNING")
+                                        self.pumpStsLED.blink(0.5,0.5,None,True)
 
-def log(msg):
-        print(msg)
+        def monitorTreeWtr(self):
+                self.log("Tree water level monitor running.")
+                level = 0
+                lastLevel = -1
+                
+                while True:
+                        # This is terrible, horrible, very bad code!
+                        if self.treeWtrLevel13.value:
+                                level = 13
+                        elif self.treeWtrLevel12.value:
+                                level = 12
+                        elif self.treeWtrLevel11.value:
+                                level = 11
+                        elif self.treeWtrLevel10.value:
+                                level = 10
+                        elif self.treeWtrLevel09.value:
+                                level = 9
+                        elif self.treeWtrLevel08.value:
+                                level = 8
+                        elif self.treeWtrLevel07.value:
+                                level = 7
+                        elif self.treeWtrLevel06.value:
+                                level = 6
+                        elif self.treeWtrLevel05.value:
+                                level = 5
+                        elif self.treeWtrLevel04.value:
+                                level = 4
+                        elif self.treeWtrLevel03.value:
+                                level = 3
+                        elif self.treeWtrLevel02.value:
+                                level = 2
+                        elif self.treeWtrLevel01.value:
+                                level = 1
+                        else:
+                                level = 0
+
+                        if lastLevel != level:
+                                self.log("Tree water level now at level " + str(level), "INFO")
+                                lastLevel = level
+
+                        level = level / 13
+                        
+                        self.wtrLevel.value = level
+
+                        sleep(1)
+
+
+def parent():
+        tree = treeWater()
         
-atexit.register(Shutdown)
+        # We should spawn these children in a better manner with IPC
+        manualRun = os.fork()
+        if manualRun == 0:
+                tree.manualRun()
+                os._exit()
+        
+        monitorWtrPump = os.fork()
+        if monitorWtrPump == 0:
+                tree.monitorWtrPump()
+                os._exit()
 
-pumpBtn.when_pressed = pumpRun
-pumpBtn.when_released = pumpStop
-#pumpSts.source = pumpBtn.values
+        monitorTreeWtr = os.fork()
+        if monitorTreeWtr == 0:
+                tree.monitorTreeWtr()
+                os._exit()
 
-for i in range(0,105, 5):
-        wtrLevel.value = i/100
-        print("i=",i)
-        sleep(1)
+        print("Waiting for children to start...")
+        sleep(5)
+        
+        while True:
+                reply = input("\nq for quit\n")
+                if reply == 'q':
+                        break
+        
+        os.kill(manualRun, signal.SIGKILL)
+        os.kill(monitorWtrPump, signal.SIGKILL)
+        os.kill(monitorTreeWtr, signal.SIGKILL)
+        tree.Shutdown()
 
-pause()
+parent()
 
-#runtime = 5.0
-#offtime = 5.0
-#loops = 0
-
-#while (True):
-#	print("Running for ",runtime," seconds.")
-#	pump.run(Adafruit_MotorHAT.FORWARD)
-#	time.sleep(runtime)
-#	print("Off for ",offtime," seconds")
-#	pump.run(Adafruit_MotorHAT.RELEASE)
-#	time.sleep(offtime)
-#	loops = loops + 1
-#	print("We have run ",loops," times.")
+#atexit.register(tree.Shutdown)
